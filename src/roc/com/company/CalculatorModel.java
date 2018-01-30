6ba7b41a3d74608a409666c5ccabd81e3f6fef49
Roeut Chak <roeut.chak@gmail.com>
2018-01-29 21:52:12 -0500

    model view controller- initial project commit

---
+++ src/roc/com/company/CalculatorModel.java
@@ -0,0 +1,14 @@
+package roc.com.company;
+
+public class CalculatorModel {
+
+    private int calculatorValue;
+
+    public void addTwoNumbers(int firstNumber, int secondNumber) {
+        calculatorValue = firstNumber + secondNumber;
+    }
+
+    public int getCalculatorValue() {
+        return calculatorValue;
+    }
+}
